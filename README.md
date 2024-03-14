e5d34e09c1962530a18a4506da2b5d54418f450c
Pema Sherpa <pematamang111@gmail.com>
2024-03-14 19:20:52 -0400

    add css

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Task-Managing-Tool
+
+
+<div class="new-container">
+      <label for="text" id="title">Task Title</label>
+      <input type="text" id="titleInput">
+      <label for="text" id="description">Description</label>
+      <textarea name="text" id="descriptionInput" cols="30" rows="10"></textarea>
+      <label for="date" id="date">Task Due Date</label>
+      <input type="date" id="dateInput">
+    </div>
--- assets/js/script.js
+++ assets/js/script.js
@@ -2,41 +2,75 @@
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
 
+//data
+const newTask = [];
+
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
-const divEl =   $("<form/>", 
-{ action:'/myaction' }
-);
-form.append( 
-$("<input>", 
-{ type:'text', 
-name:'title', 
-style:'width:30%' }
-)
-);
-
-form.append( 
-$("<input>", 
-{ type:'date', 
-value:'date', 
-style:'width:30%' }
-)
-);
-
-$("#someDivId").append(form);
+    const uniqueId = Math.floor(Math.random()*1000000000);
+    return(uniqueId);
 }
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
+    
+    const newCard = document.createElement('div');
+    newCard.className = 'card project-card draggable my-3';
+    newCard.attr('class', 'data-project-id');
 
 
+    const newHead = newCard.createElement('h2');
+    newHead.innerHTML = 'Add Class';
+    newHead.className = 'card-header';
+    newCard.appendChild(newHead);
 
+    const body = document.createElement('body');
+    newCard.appendChild(body);
+
+    const newTitle = body.createElement('h5');
+    newTitle.innerHTML = 'Title';
+    newTitle.className = 'title';
+    newCard.appendChild(newTitle);
+
+    const newTitleInput = body.createElement('input');
+    newCard.appendChild(newTitleInput);
+
+    const newDescription = document.createElement('h5');
+    newDescription.textContent = 'Description';
+    newCard.appendChild(newDescription)
+    
+    const newDescriptionInput = body.createElement('input');
+    newCard.appendChild(newDescriptionInput);
+
+    const newCalender = document.createElement('h5');
+    newCalender.textContent = 'Due Date';
+    const newCalenderInput = document.createElement('input');
+    newCalenderInput.className = 'calender';
+    $( ".calender" ).datepicker();
+    newCard.appendChild(newCalender);
+
+    const newButton = body.createElement('button');
+    newButton.className = 'btn-btn';
+    newButton.textContent = 'Delete';
+    newCard.appendChild(newButton); 
+
+    return newCard;
+   
 
 }
 
 // Todo: create a function to render the task list and make cards draggable
 function renderTaskList() {
+   const todo = document.querySelector('.todo-cards');
+   
+   for (let i = 0; i<newTask.length; i++){
+      const newCard = createTaskCard(task);
+      todo.appendChild(newCard);
+   }
 
+   $( function() {
+    $( ".draggable" ).draggable();
+  } );
 }
 
 // Todo: create a function to handle adding a new task
